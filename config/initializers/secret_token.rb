516d63869f1e5b7c442809b6ab9bddd8b5ade964
devudilip <devu.dilip@gmail.com>
2014-07-06 23:56:02 +0530

    initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordCorrectness::Application.config.secret_token = 'e0d56c741e9b74fd5edf82d57985a9707367000c3090ce4f30a999169862d30b234d16d11642e348dd786915732e71a8ba3e0df9a82ca948b2f039705fec1907'
